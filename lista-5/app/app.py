6ac8b3eb7044cd2ce537b9873ed190a24be921ae
Vaux Gomes <vauxsandino@gmail.com>
2025-01-31 22:12:30 -0300

    Lista 5: 20% dos dados

--- lista-5/app/app.py
+++ lista-5/app/app.py
@@ -1,7 +1,12 @@
 # Libs
 import pandas as pd
-from flask import Flask, render_template, request
 from module.tfidf import TFIDF
+from flask import Flask, render_template, request
+
+from wordcloud import WordCloud, STOPWORDS
+import base64
+from io import BytesIO
+from PIL import Image
 
 # App
 app = Flask(__name__)
@@ -12,36 +17,70 @@ tfidf.load()
 
 # Documents
 df = pd.read_csv('./files/documents.csv')
-df = df.dropna().reset_index(drop=True)
+df = df.dropna().reset_index(drop=True).sample(frac=0.6, random_state=42).reset_index(drop=True)
 
 # Index route
 @app.route("/", methods=["GET", "POST"])
 def index():
   results = []
-  term = ""
+  terms = ""
   
   # Search config
   top = 10
   all = True
+  
+  # Image
+  img_str = ''
 
   #
   if request.method == "POST":
-    term = request.form.get("term").lower()
-    
-    if term:
+    terms = request.form.get("terms").lower()
+    terms = [term.strip() for term in terms.split(';')]
+
+    if len(terms) > 0:
       top = int(request.form.get("top"))
       all = request.form.get("all")
-              
-      scores = tfidf.tfidf(term, all=all)[:top]
-      
-      for score, document_id in scores:
-        results.append({
-          "document_id": document_id,
-          "document_text": df.iloc[document_id].text,
-          "score": score
-        })
+      documents = []
 
-  return render_template("index.html", results=results, term=term, top=top, all=all)
+      for term in terms:
+        search = tfidf.tfidf(term, all=all)
+        scores = search[:top]
+      
+        # For each search feedback
+        for score, document_id in scores:
+          results.append({
+            "document_id": document_id,
+            "document_text": df.iloc[document_id].text,
+            "score": score,
+            "term": term
+          })
+        
+          documents.append(df.iloc[document_id].text)
+      
+      # Resorting
+      results = sorted(results, key=lambda item: item['score'], reverse=True)
+
+      # Wordcloud
+      wc = WordCloud(
+        max_words=200, stopwords=set(STOPWORDS),
+        width=1296, height=550,
+        min_font_size=4, max_font_size=150,
+        mode='RGBA', background_color=None
+      )
+      
+      #
+      wc.generate(' '.join(documents))
+      wc.to_file("images/nuvem.png")
+      
+      #
+      img = Image.open("images/nuvem.png")
+      
+      # Converting to base64
+      buffered = BytesIO()
+      img.save(buffered, format="PNG") # Escolha o formato adequado
+      img_str = base64.b64encode(buffered.getvalue()).decode()
+
+  return render_template("index.html", results=results, terms='; '.join(terms), top=top, all=all, img_str=img_str)
 
 # Main
 if __name__ == "__main__":
